d2a76dfb4e477b1f723936f85c84cf4dde7cb436
Boldijar Paul <boldijar.paul@gmail.com>
2016-01-26 10:05:08 -0800

    you can now load image

--- app/src/main/java/com/boldijar/image_upload_demo/UploadActivity.java
+++ app/src/main/java/com/boldijar/image_upload_demo/UploadActivity.java
@@ -1,13 +1,106 @@
 package com.boldijar.image_upload_demo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class UploadActivity extends AppCompatActivity {
+import java.io.File;
+
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int SELECT_PHOTO = 69;
+    private static final int PERMISSION_REQUEST = 12;
+
+    // the views
+    private ImageView mImageView;
+    private Button mButton;
+    private File mFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        // create views, add event listeners
+        mImageView = (ImageView) findViewById(R.id.imageview);
+        mButton = (Button) findViewById(R.id.button);
+        mImageView.setOnClickListener(this);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!gotStoragePermission()) {
+            askStoragePermission();
+            return;
+        }
+        Intent intent = new Intent(
+                Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECT_PHOTO);
+        // start intent to get image
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            mImageView.setImageURI(selectedImage);
+
+            String imageUrl = getRealPathFromUri(selectedImage);
+            mFile = new File(imageUrl);
+        }
+    }
+
+    /**
+     * @param contentUri the uri of the file
+     * @return the absolute path of image
+     */
+    public String getRealPathFromUri(Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    /**
+     * @return if we accepted the storage permission or not
+     */
+    private boolean gotStoragePermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /**
+     * ask for permission
+     */
+    private void askStoragePermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE},
+                PERMISSION_REQUEST);
     }
 }
